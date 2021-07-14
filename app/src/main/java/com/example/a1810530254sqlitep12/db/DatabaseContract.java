faedf2e3a0f0665bfca435ec9db1c99fb90e4881
hariswp-16 <wahyupretetet004@gmail.com>
2021-07-14 11:41:04 +0800

    NotesApp

---
+++ app/src/main/java/com/example/a1810530254sqlitep12/db/DatabaseContract.java
@@ -0,0 +1,21 @@
+package com.example.a1810530254sqlitep12.db;
+
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+
+
+
+     public static final class NoteColumns implements BaseColumns {
+
+         public static final String TABLE_NAME = "note";
+
+        //Note Title
+        public static final String TITLE = "title";
+        //Note description
+        public static final String DESCRIPTION = "description";
+        //Note date
+        public static final String DATE = "date";
+    }
+
+}
---
+++ app/src/main/java/com/example/a1810530254sqlitep12/helper/MappingHelper.java
@@ -0,0 +1,27 @@
+package com.example.a1810530254sqlitep12.helper;
+
+import android.database.Cursor;
+
+import com.example.a1810530254sqlitep12.Entity.Note;
+import com.example.a1810530254sqlitep12.db.DatabaseContract;
+
+import java.util.ArrayList;
+
+public class MappingHelper {
+
+    public static ArrayList<Note> mapCursorToArrayList(Cursor notesCursor) {
+        ArrayList<Note> notesList = new ArrayList<>();
+
+        while (notesCursor.moveToNext()) {
+            int id = notesCursor.getInt(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID));
+            String title = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE));
+            String description = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION));
+            String date = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE));
+            notesList.add(new Note(id, title, description, date));
+        }
+
+        return notesList;
+    }
+
+
+}
